9abd9d43416de8e04a199d93698465a29cf7e027
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-01 23:01:22 +0200

    feat: add LoginUserDto type

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -11,13 +11,20 @@ export let registerUser = async (userData: RegisterUserDto): Promise<void> => {
 };
 
 //get access token - no user data is passed here
+export type LoginUserDto = {
+  email: string;
+  password: string;
+};
+
 export let loginUser = async (
-  userData: LoginUserDto,
+    userData: LoginUserDto,
 ): Promise<AuthResponse> => {
   let res = await api.post<AuthResponse>("/api/security/login", userData);
   setAccessToken(res.data.accessToken);
   return res.data;
 };
+
+
 
 //calling logout so backend will clear refresh token from browser
 export let logoutUser = async (): Promise<void> => {
