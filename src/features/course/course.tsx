32624cb5fedd611b2fe1cb361aa9252be838f81b
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-01 23:02:02 +0200

    refactor: extend Course and related interfaces

--- src/features/course/course.tsx
+++ src/features/course/course.tsx
@@ -2,13 +2,38 @@ export interface Course {
   id: string;
   name: string;
   description: string;
-  teacherId: string;
   variants: CourseVariant[];
   thumbnailUrl: string;
+  teacher: Teacher;
+  courseBrief: CoursesBrief[];
+  teacherReviews: TeacherReview[];
+  teacherAvailability: TeacherAvailability[];
 }
 
 export interface CourseVariant {
   level: string;
   price: number;
   language: string;
+}
+export interface Teacher {
+  name: string;
+  surname: string;
+  description: string;
+  coursesBrief:[];
+}
+
+export interface CoursesBrief {
+  id: string;
+  name: string
+}
+
+export interface TeacherReview {
+  reviewerName: string;
+  reviewerSurname: string;
+  starsNumber: number;
+  content: string
+}
+export interface TeacherAvailability {
+  day: string;
+  timeslots: { timeFrom: string; timeUntil: string }[];
 }
