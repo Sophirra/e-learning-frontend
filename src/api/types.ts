33317ced8cb0058efaf93663102fc6232cf04738
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-09-01 23:58:32 +0200

    refactor: move all course- and teacher-related TypeScript types to a centralized types.ts file

--- src/api/types.ts
+++ src/api/types.ts
@@ -25,3 +25,45 @@ export interface aboutUser {
   telephone: string;
   description: string;
 }
+
+// Teacher Section
+export interface Teacher {
+  name: string;
+  surname: string;
+  description: string;
+  coursesBrief:[];
+}
+
+export interface TeacherReview {
+  reviewerName: string;
+  reviewerSurname: string;
+  starsNumber: number;
+  content: string
+}
+
+export interface TeacherAvailability {
+  day: string;
+  timeslots: { timeFrom: string; timeUntil: string }[];
+}
+
+// Course Section
+export interface Course {
+  id: string;
+  name: string;
+  description: string;
+  variants: CourseVariant[];
+  thumbnailUrl: string;
+  teacher: Teacher;
+}
+
+export interface CourseVariant {
+  level: string;
+  price: number;
+  language: string;
+}
+
+export interface CoursesBrief {
+  id: string;
+  name: string
+}
+
