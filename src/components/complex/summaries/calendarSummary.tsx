0dc0583eec9ed1ccc0867a9818f54e10cd7ad725
Mikołaj Solarz <m.solarz@sscentrum.pl>
2025-10-07 23:09:54 +0200

    feat: connect CalendarSummary to API and enable course navigation

--- src/components/complex/summaries/calendarSummary.tsx
+++ src/components/complex/summaries/calendarSummary.tsx
@@ -1,25 +1,36 @@
 import { iconLibrary as icons } from "@/components/iconLibrary.tsx";
 import Summary from "@/components/complex/summaries/summary.tsx";
-import type { CourseBrief } from "@/components/complex/studentDetailsCard.tsx";
 import { Button } from "@/components/ui/button.tsx";
 import { Label } from "@/components/ui/label.tsx";
+import type {CourseBrief} from "@/api/types.ts";
+import {useNavigate} from "react-router-dom";
 
 export function CalendarSummary({ courses }: { courses: CourseBrief[] }) {
+  const navigate = useNavigate();
+
   return (
-    <Summary label={"Calendar"} labelIcon={icons.Calendar} canHide={true}>
-      {courses.map((course: CourseBrief) => (
-        <div
-          className={"flex flex-row gap-0"}
-          key={course.id}
-          style={{ width: "100%" }}
-        >
-          <Button variant={"link"} className={"w-300px"}>
-            {course.name}:
-          </Button>
-          {/*HERE GET COURSE UPCOMING CLASSES*/}
-          <Label>Class upcoming in 2 days</Label>
-        </div>
-      ))}
-    </Summary>
+      <Summary label="Calendar" labelIcon={icons.Calendar} canHide={true}>
+        {courses.map((course) => (
+            <div
+                key={`${course.courseId}}`}
+                className="flex flex-row gap-0"
+                style={{ width: "100%" }}
+            >
+              <Button
+                  variant="link"
+                  className="w-300px"
+                  onClick={() =>
+                      navigate(`/course/${course.courseId}`, {
+                        state: { teacherId: course.teacherId ?? "" },
+                      })
+                  }
+              >
+                {course.courseName}
+              </Button>
+              <Label>{course.startTime.toLocaleString()}</Label>
+            </div>
+        ))}
+      </Summary>
   );
 }
+
