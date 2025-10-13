270cd24a7047c8b730fa1639e3fd6734167fa7b3
cpetryka <cpetryka@griddynamics.com>
2025-10-13 22:39:35 +0200

    feat: implement skeleton for assignments page

---
+++ src/components/complex/assignmentPageContent/AssignmentGrade.tsx
@@ -0,0 +1,35 @@
+import {iconLibrary as icons} from "@/components/iconLibrary.tsx";
+import Summary from "@/components/complex/summaries/summary.tsx";
+import {Label} from "@/components/ui/label.tsx";
+import type {AssignmentBrief} from "@/pages/UserPages/AssignmentPage.tsx";
+
+export function AssignmentGrade({
+                                    assignment,
+                                }: {
+    assignment: AssignmentBrief;
+}) {
+    return (
+        <Summary
+            label={"Grades"}
+            labelIcon={icons.PenTool}
+            canHide={false}
+        >
+            <div className="flex flex-col gap-2 p-2 text-xs font-normal ml-1">
+                {assignment.graded && assignment.grade !== undefined ? (
+                    <div
+                        className="flex flex-row gap-0 ml-2 mt-2"
+                    >
+                        <Label className="mr-4">
+                            {assignment.grade}
+                        </Label>
+                        <Label className="font-light">
+                            {assignment?.comments}
+                        </Label>
+                    </div>
+                ) : (
+                    <Label className="mt-2 ml-2 font-light">No grade yet.</Label>
+                )}
+            </div>
+        </Summary>
+    );
+}
